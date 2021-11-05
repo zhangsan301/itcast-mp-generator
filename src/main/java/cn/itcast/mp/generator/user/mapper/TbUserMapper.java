11d207a209333e17fe53eca5286015713fd0b1d6
zhangsan_301 <3152319511@qq.com>
2021-11-05 17:25:54 +0800

    这是一个基于mybatisplus的代码生成器程序

---
+++ src/main/java/cn/itcast/mp/generator/user/mapper/TbUserMapper.java
@@ -0,0 +1,16 @@
+package cn.itcast.mp.generator.user.mapper;
+
+import cn.itcast.mp.generator.user.entity.TbUser;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ * @since 2021-11-05
+ */
+public interface TbUserMapper extends BaseMapper<TbUser> {
+
+}
